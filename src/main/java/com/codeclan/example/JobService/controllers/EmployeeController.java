006aacb79071d11a2bdf17ebb84702cb7aa5fede
Alexander Seton <alexanderseton@protonmail.com>
2022-01-18 11:59:42 +0000

    Couple routes created

--- src/main/java/com/codeclan/example/JobService/controllers/EmployeeController.java
+++ src/main/java/com/codeclan/example/JobService/controllers/EmployeeController.java
@@ -1,7 +1,29 @@
-// package com.codeclan.example.JobService.controllers;
+package com.codeclan.example.JobService.controllers;
 
-// import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import java.util.Optional;
 
-// public class EmployeeController {
+import com.codeclan.example.JobService.models.Employee;
+import com.codeclan.example.JobService.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-// }
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @GetMapping(value = "/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @GetMapping(value = "/employees/{id}")
+    public Optional<Employee> getEmployeeById(@PathVariable long id) {
+        return employeeRepository.findById(id);
+    }
+
+}
